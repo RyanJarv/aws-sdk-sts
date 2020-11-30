fd94c8acfd321dc2d10ff4d43638463867de1a37
RyanJarv <ryan_gerstenkorn@fastmail.fm>
2020-11-30 05:30:44 -0600

    Raise error and fix tests

--- lib/aws-sdk-sts/client.rb
+++ lib/aws-sdk-sts/client.rb
@@ -3,7 +3,9 @@ module Aws; end
 module Aws::STS
     class Client
         def initialize
-            puts "!!!!! This is not the real aws-sdk library, you probably meant to install 'aws-sdk-iam' instead"
+            msg = "!!!!! This is not the real aws-sdk library, you probably meant to install 'aws-sdk-iam' instead"
+            $stderr.puts msg
+            raise msg
         end
     end
 end
--- spec/aws/sdk/sts_spec.rb
+++ spec/aws/sdk/sts_spec.rb
@@ -3,7 +3,11 @@ RSpec.describe Aws::STS do
     expect(Aws::STS::GEM_VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(Aws::STS::Client.new).not_to be nil
+  it "outputs warning" do
+    expect { Aws::STS::Client.new }.to raise_error().and output(/not the real aws-sdk library/).to_stderr
+  end
+
+  it "raises an error" do
+    expect { Aws::STS::Client.new }.to raise_error(/not the real aws-sdk library/)
   end
 end
